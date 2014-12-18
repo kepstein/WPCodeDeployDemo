80d424fbcb54676dba2e71fb70a129344c2a5214
EC2 Default User <ec2-user@ip-172-31-0-200.us-west-2.compute.internal>
2014-12-18 16:12:54 +0000

    Updated permissions script

--- scripts/change_permissions.sh
+++ scripts/change_permissions.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-chmod -R 644 /var/www/html/WordPress
+chown ec2-user:apache /var/www/html/WordPress
+find /var/www/html/WordPress -type f -exec chmod 644 {} \;
+find /var/www/html/WordPress -type d -exec chmod 754 {} \;
+#chmod -R 644 /var/www/html/WordPress
