f9b5dafce7e8a50c685921f6ec2eaa04f90490d8
Kevin Epstein <kevin@epstein.co.za>
2015-03-02 08:38:54 -0800

    Added new logic to stop script, and fixed bug in if statements

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 isExistApp=`pgrep httpd`
-if [[ -n  \$isExistApp ]]; then
+if [[ -n  $isExistApp ]]; then
    service httpd stop
+else 
+	echo "The httpd service is not running. No action taken"
 fi
+
 isExistApp=`pgrep mysqld`
-if [[ -n  \$isExistApp ]]; then
+if [[ -n  $isExistApp ]]; then
     service mysqld stop
 else 
-	echo "mysqld service not found, failing gracefully to allow scripts to continue"
+	echo "The mysqld service is not running. No action taken"
 fi
