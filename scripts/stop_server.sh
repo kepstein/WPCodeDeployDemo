8249db5508a5423290b53ed4778f20a013e5702e
Kevin Epstein <kevin@epstein.co.za>
2015-03-02 08:32:27 -0800

    Changed stop script logic to allow to fail gracefully and allow the scripts to continue if mysqld does not exist

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -3,7 +3,9 @@ isExistApp=`pgrep httpd`
 if [[ -n  \$isExistApp ]]; then
    service httpd stop
 fi
-#isExistApp=`pgrep mysqld`
-#if [[ -n  \$isExistApp ]]; then
-#    service mysqld stop
-#fi
+isExistApp=`pgrep mysqld`
+if [[ -n  \$isExistApp ]]; then
+    service mysqld stop
+else 
+	echo "mysqld service not found, failing gracefully to allow scripts to continue"
+fi
